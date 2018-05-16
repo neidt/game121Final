2bf4325654e1d62c70e87661c8da58fd74489cf8
svc-student <svc-student@student.directory.jccc>
2018-05-16 15:49:46 -0500

    final for Natalie Eidt

--- Assets/MovingObstacle.cs
+++ Assets/MovingObstacle.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingObstacle : MonoBehaviour {
-
+public class MovingObstacle : MonoBehaviour
+{
+    public LayerMask movingObst;
     public float moveSpeed = 3.0f;
+    public Transform start;
+    public Transform stop;
+    public bool isAtStart = false;
+    public bool isAtStop = false;
+    // Use this for initialization
+    void Start()
+    {
+        isAtStart = true;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Ray intoScreen = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (isAtStart)
+        {
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
+        }
+        else if (isAtStop)
+        {
+            transform.position += -transform.right * moveSpeed * Time.deltaTime;
+        }
+
+        if (Physics.Raycast(intoScreen, out hitInfo, 1000, movingObst))
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isAtStart)
+            {
+                isAtStart = false;
+                isAtStop = true;
+            }
+            else if(Input.GetKeyDown(KeyCode.Space) && isAtStop)
+            {
+                isAtStop = false;
+                isAtStart = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().transform == start)
+        {
+            isAtStop = false;
+            isAtStart = true;
+            
+        }
+        else if (other.GetComponent<Collider>().transform == stop)
+        {
+            isAtStart = false;
+            isAtStop = true;
+        }
+    }
 }
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -2,16 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour {
+public class PlayerInput : MonoBehaviour
+{
 
     public float moveSpeed = 5.0f;
+    public Transform player;
+    public float radiusOfSat = .05f;
+    private Vector3 target;
+    public float rayDist = 1f;
+    public LayerMask floorOnly;
+    private bool hitThisFrame = false;
+    private Vector3 hitLocThisFrame = Vector3.zero;
+    public int pickupCount = 4;
+    public Vector3 offsetVec = new Vector3(0, 1, 0);
+    public Vector3 rotatePoint;
+    public Camera cam;
+    public Camera sceneCam;
+    public GameObject movingObst;
+    // Use this for initialization
+    void Start()
+    {
+        cam.enabled = false;
+    }
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        float step = moveSpeed * Time.deltaTime;
+        Ray intoScreen = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(intoScreen, out hitInfo, 1000, floorOnly))
+            {
+                target = hitInfo.point;
+                player.LookAt(target + offsetVec);
+            }
+        }
 
+        if (Physics.Raycast(intoScreen, out hitInfo, 1000, floorOnly))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+            }
+        }
+
+            if (Vector3.Distance(player.position, target) > radiusOfSat)
+        {
+            transform.position = Vector3.MoveTowards(player.transform.position, target + offsetVec, step);
+        }
+
+        if(pickupCount == 0)
+        {
+            sceneCam.enabled = false;
+            cam.enabled = true;
+            cam.transform.Rotate(Vector3.up, 1f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "pickup")
+        {
+            other.gameObject.SetActive(false);
+            pickupCount--;
+        }
     }
 }
